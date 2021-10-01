c91178e11f145f2447140879794b01bb284094ab
HT002 <79648523+HT002@users.noreply.github.com>
2021-10-01 21:27:57 +0700

    Update zji.php

--- zji.php
+++ zji.php
@@ -1,1 +1,5 @@
-<div style="background-image:url('https://user-images.githubusercontent.com/79648523/135633500-a7410dd5-d9f8-404a-bc17-b2595a1fa6b4.png');padding:5px;width:150px;height:200px;border:1px solidsolidsolidsolidsolidsolsolidsolidsolidsolidsolidsolidsolidsolididsolidsolid black;">
+<div style="background-image:url('https://raw.githubusercontent.com/HT002/KAGAMI1/main/rotate-pulsating-loading-animation.gif');padding:5px;width:150px;height:200px;border:1px solid black;">
+  
+  <p>See the background image?</p>
+   
+    </div>
