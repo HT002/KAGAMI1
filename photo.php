1ad42412784c8b72b9d7a84f90ba1b003f593a3a
HT002 <79648523+HT002@users.noreply.github.com>
2021-10-01 20:45:53 +0700

    Update photo.php

--- photo.php
+++ photo.php
@@ -1,2 +1,19 @@
 
 ![ezgif.com-gif-maker.gif](https://user-images.githubusercontent.com/79648523/135629894-62ad9bc1-26fd-41de-b77c-21252ee49db1.gif)
+<script language="javascript">
+
+function js_popup(theURL,width,height) { //v2.0
+
+leftpos = (screen.availWidth - width) / 2;
+
+     toppos = (screen.availHeight - height) / 2;
+
+   window.open(theURL, "viewdetails","width=" + width + ",height=" + height + ",left=" + leftpos + ",top=" + toppos);
+
+}
+
+</script>
+
+ 
+
+<a href="#" onClick="js_popup('test2.php',783,600); return false;" title="Code PHP Popup">Test Code PHP Popup</a>
