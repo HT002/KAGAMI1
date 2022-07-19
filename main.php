ffb528e8287cde973ead68d764e0841d072a9c63
HT002 <79648523+HT002@users.noreply.github.com>
2022-07-19 22:59:05 +0700

    Create main.php

---
+++ main.php
@@ -0,0 +1,29 @@
+{"Version":"1.1",
+
+"Url":"https://github.com/HT002/KAGAMI1/blob/main/CODE%20A.zip?raw=true" ,
+
+"Changelog":"  
+
+test vpn server
+
+✅What we privode in our app
+
+AIS DTAC TRUE
+
+Hope you all enjoy and thank you for using our app
+
+",
+
+"latestVersion": "1.0.0",
+
+"latestVersionCode": "1",
+
+"url": "",
+
+ "releaseNotes": [
+
+ "-Demo"
+
+  ]
+
+}
